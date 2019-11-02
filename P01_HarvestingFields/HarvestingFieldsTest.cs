45d57501151f5fb1a73b85f3c59c1a7da82ba610
Svetlin Stoyanov <svetlinst@gmail.com>
2019-11-02 20:37:36 +0200

    Add project files.

---
+++ P01_HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,42 @@
+﻿ namespace P01_HarvestingFields
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            var classType = typeof(HarvestingFields);
+            var bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+            var fields = classType.GetFields(bindingFlags);
+            string input;
+            while ((input=Console.ReadLine()) != "HARVEST")
+            {
+                switch (input)
+                {
+                    case "private":
+                        foreach (var field in fields.Where(f=>f.IsPrivate == true))
+                        {
+                            Console.WriteLine($"private {field.FieldType.Name} {field.Name}");
+                        }
+                        break;
+                    case "public":
+                        foreach (var field in fields.Where(f => f.IsPublic == true))
+                        {
+                            Console.WriteLine($"public {field.FieldType.Name} {field.Name}");
+                        }
+                        break;
+                    case "protected":
+                        foreach (var field in fields.Where(f => f.IsFamily == true))
+                        {
+                            Console.WriteLine($"protected {field.FieldType.Name} {field.Name}");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ P02_BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,26 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            var classType = typeof(BlackBoxInteger);
+            var classInstance = Activator.CreateInstance(classType, true);
+            var bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+
+            string input;
+            while((input = Console.ReadLine()) != "END")
+            {
+                var tokens = input.Split('_', StringSplitOptions.RemoveEmptyEntries);
+                var method = classType.GetMethod(tokens[0], bindingFlags);
+                method.Invoke(classInstance, new object[] { int.Parse(tokens[1])});
+                var result = classType.GetField("innerValue", bindingFlags).GetValue(classInstance);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
